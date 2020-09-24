879b2072397401ce1ba5a1e24366290195e6313b
Tim Van Holder <tim.vanholder@gmail.com>
2020-09-24 20:24:20 +0200

    Update version references (2.25x -> 2.258)

--- src/test/java/org/jenkinsci/plugins/workflow/steps/CoreStepTest.java
+++ src/test/java/org/jenkinsci/plugins/workflow/steps/CoreStepTest.java
@@ -207,15 +207,15 @@ public class CoreStepTest {
         @DataBoundConstructor
         public BuilderWithWorkspaceRequirement() {
         }
-        // TODO: Remove once the minimum core version for this plugin is 2.25x or newer.
+        // TODO: Remove once the minimum core version for this plugin is 2.258 or newer.
         public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull Launcher launcher, @Nonnull TaskListener listener) throws InterruptedException, IOException {
             fail("This method should not get called.");
         }
-        // TODO: Mark as @Override once the minimum core version for this plugin is 2.25x or newer.
+        // TODO: Mark as @Override once the minimum core version for this plugin is 2.258 or newer.
         public void perform(@Nonnull Run<?, ?> run, @Nonnull EnvVars env, @Nonnull TaskListener listener) throws InterruptedException, IOException {
             listener.getLogger().println("workspace context required, but not provided!");
         }
-        // TODO: Mark as @Override once the minimum core version for this plugin is 2.25x or newer.
+        // TODO: Mark as @Override once the minimum core version for this plugin is 2.258 or newer.
         public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull EnvVars env, @Nonnull Launcher launcher, @Nonnull TaskListener listener) throws InterruptedException, IOException {
             listener.getLogger().println("workspace context required and provided.");
         }
@@ -247,19 +247,19 @@ public class CoreStepTest {
         @DataBoundConstructor
         public BuilderWithoutWorkspaceRequirement() {
         }
-        // TODO: Remove once the minimum core version for this plugin is 2.25x or newer.
+        // TODO: Remove once the minimum core version for this plugin is 2.258 or newer.
         public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull Launcher launcher, @Nonnull TaskListener listener) throws InterruptedException, IOException {
             fail("This method should not get called.");
         }
-        // TODO: Mark as @Override once the minimum core version for this plugin is 2.25x or newer.
+        // TODO: Mark as @Override once the minimum core version for this plugin is 2.258 or newer.
         public void perform(@Nonnull Run<?, ?> run, @Nonnull EnvVars env, @Nonnull TaskListener listener) throws InterruptedException, IOException {
             listener.getLogger().println("workspace context not needed.");
         }
-        // TODO: Mark as @Override once the minimum core version for this plugin is 2.25x or newer.
+        // TODO: Mark as @Override once the minimum core version for this plugin is 2.258 or newer.
         public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull EnvVars env, @Nonnull Launcher launcher, @Nonnull TaskListener listener) throws InterruptedException, IOException {
             listener.getLogger().println("workspace context not needed, but provided.");
         }
-        // TODO: Mark as @Override once the minimum core version for this plugin is 2.25x or newer.
+        // TODO: Mark as @Override once the minimum core version for this plugin is 2.258 or newer.
         public boolean requiresWorkspace() {
             return false;
         }
