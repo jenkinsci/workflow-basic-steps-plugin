4a93a4cf7dc450649b749705c901dbea8f9299a3
Nikolas Falco <nikolas.falco@finantix.com>
2022-01-28 15:01:49 +0100

    [JENKINS-67697] Create a step to set the result of the current stage
    Add stageResult step to setup only the result of the current stage.

---
+++ src/main/java/org/jenkinsci/plugins/workflow/steps/StageResultStep.java
@@ -0,0 +1,125 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2022 Falco Nikolas
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jenkinsci.plugins.workflow.steps;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.jenkinsci.plugins.workflow.actions.WarningAction;
+import org.jenkinsci.plugins.workflow.graph.FlowNode;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import hudson.Util;
+import hudson.model.Result;
+import hudson.model.TaskListener;
+import hudson.util.FormValidation;
+import hudson.util.ListBoxModel;
+
+public class StageResultStep extends Step {
+
+    private final String message;
+    private final String result;
+
+    @DataBoundConstructor
+    public StageResultStep(String message, String result) {
+        message = Util.fixEmptyAndTrim(message);
+        if (message == null) {
+            throw new IllegalArgumentException("A non-empty message is required");
+        }
+        this.message = message;
+
+        result = Util.fixEmptyAndTrim(result);
+        if (result == null || !result.equalsIgnoreCase(Result.fromString(result).toString())) {
+            throw new IllegalArgumentException("result is invalid: " + result + ". Valid options are SUCCESS, UNSTABLE, FAILURE, NOT_BUILT and ABORTED.");
+        }
+        this.result = result;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public StepExecution start(StepContext context) throws Exception {
+        return new StageResultStepExecution(this, context);
+    }
+
+    private static class StageResultStepExecution extends SynchronousStepExecution<Void> {
+        private static final long serialVersionUID = 1L;
+        private transient final StageResultStep step;
+
+        private StageResultStepExecution(StageResultStep step, StepContext context) {
+            super(context);
+            this.step = step;
+        }
+
+        @Override
+        protected Void run() throws Exception {
+            getContext().get(FlowNode.class).addOrReplaceAction(new WarningAction(Result.fromString(step.result)).withMessage(step.message));
+            getContext().get(TaskListener.class).getLogger().println(step.message);
+            return null;
+        }
+    }
+
+    @Extension
+    public static class DescriptorImpl extends StepDescriptor {
+        @Override
+        public String getFunctionName() {
+            return "stageResult";
+        }
+
+        @NonNull
+        @Override
+        public String getDisplayName() {
+            return "Set result of the current stage";
+        }
+
+        @Override
+        public Set<? extends Class<?>> getRequiredContext() {
+            Set<Class<?>> context = new HashSet<>();
+            Collections.addAll(context, FlowNode.class, TaskListener.class);
+            return Collections.unmodifiableSet(context);
+        }
+
+        public FormValidation doCheckMessage(@QueryParameter String message) {
+            if (Util.fixEmptyAndTrim(message) == null) {
+                return FormValidation.error("Message must be non-empty");
+            }
+            return FormValidation.ok();
+        }
+
+        public ListBoxModel doFillResultItems() {
+            ListBoxModel r = new ListBoxModel();
+            for (Result result : Arrays.asList(Result.SUCCESS, Result.UNSTABLE, Result.FAILURE, Result.NOT_BUILT, Result.ABORTED)) {
+                r.add(result.toString());
+            }
+            return r;
+        }
+    }
+}
